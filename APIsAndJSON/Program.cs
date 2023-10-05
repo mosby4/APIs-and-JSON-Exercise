18e920508644e9b425971e7f7c86144d0607f441
Mosby4 <rufus.hamby@gmial.com>
2023-10-05 10:53:30 -0500

    First attempt

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace APIsAndJSON
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using static System.Net.WebRequestMethods;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Kanye: {RonVSKanyeAPI.kanyeQuote()}");
+                Console.WriteLine();
+                Console.WriteLine($"Ron:{RonVSKanyeAPI.ronQuote()}");
+            }
+
         }
     }
 }
--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace APIsAndJSON
 {
-    internal class RonVSKanyeAPI
+    public class RonVSKanyeAPI
     {
+        public static string kanyeQuote()
+        {
+            var client = new HttpClient();
+
+            var kanyeURL = "https://api.kanye.rest";
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            return kanyeQuote;
+        }
+
+        public static string ronQuote()
+        {
+            var client = new HttpClient();
+
+            var ronResponse = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            return ronQuote;
+        }
+
     }
 }
